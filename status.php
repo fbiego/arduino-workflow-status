d17d653d0edeeb92c5fe6b29d25f1c186e6fa954
Felix Biego <45796740+fbiego@users.noreply.github.com>
2022-08-07 13:03:05 +0300

    Create status.php

---
+++ status.php
@@ -0,0 +1,19 @@
+<?php
+
+function sendStatus($bot_token, $chat_id, $status)
+{
+    $cURLConnection = curl_init();
+    curl_setopt(
+        $cURLConnection,
+        CURLOPT_URL,
+        "https://api.telegram.org/bot" . $bot_token . "/sendMessage?chat_id=" .$chat_id . "&parse_mode=html&text=" . urlencode($status)
+    );
+    curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+    $json = curl_exec($cURLConnection);
+    curl_close($cURLConnection);
+	
+}
+
+
+sendStatus($argv[1], $argv[2], $argv[3]);
+?>
